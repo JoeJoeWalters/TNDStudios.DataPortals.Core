6f9ef2a591983d0e2222c27c55f9d2710336ee51
TNDStudios <info@thenakeddeveloper.com>
2018-09-14 00:20:30 +0100

    Fleshing out the tests so we can check different scenarios. Properly defined the scopt of each of the first 2 tests of flat files.

--- FlatFileProvider.Tests/Tests.cs
+++ FlatFileProvider.Tests/Tests.cs
@@ -9,42 +9,98 @@ namespace TNDStudios.DataPortals.Tests.Providers
 {
     public class FlatFileProviderTests
     {
+        // Constants for the test files (used so we can get the resouce stream
+        // but also so we can abstract the creation of the data definition)
+        private const String testFile_DataTypes = "TestFiles.DataTypesTest.txt";
+        private const String testFile_Headers = "TestFiles.HeadersTest.txt";
+
+        /// <summary>
+        /// Test to make sure that each column correctly identifies a
+        /// different data type when reading from a flat file 
+        /// There are other tests for format specific items (such as date formats)
+        /// </summary>
         [Fact]
-        public void Read_FlatFile_From_Stream()
+        public void DataTypes_From_FileStream()
         {
             // Arrange: Get the test data from the resource in the manifest
-            Stream resourceStream = GetResourceStream("TestFiles.CSVTestWithHeaders.txt");
+            Stream resourceStream = GetResourceStream(testFile_DataTypes);
 
             // Arrage: Generate a new flat file provider and then connect it to the stream of the CSV file
             IDataProvider provider = new FlatFileProvider();
             provider.Connect(resourceStream);
 
             // Arrage: Provide a definition of what wants to be retrieved from the flat file
-            DataItemDefinition definition = TestDefinition(true);
+            DataItemDefinition definition = TestDefinition(testFile_DataTypes);
 
             // Act
-            DataTable data = provider.ExecuteReader(definition, "");
+            DataTable data = provider.ExecuteReader(definition, ""); // Get the data
 
             // Assert
-            Assert.True(data.Rows.Count != 0);
+            Assert.True(data.Rows.Count != 0); // It actually got some data rows
+        }
+
+        /// <summary>
+        /// Test to make sure that headers are picked up when the file is identified 
+        /// as having a header and that the quoted and unquoted header definitions
+        /// pick up data from the correct columns
+        /// </summary>
+        [Fact]
+        public void DataFromHeaders_From_FileStream()
+        {
+            // Arrange: Get the test data from the resource in the manifest
+            Stream resourceStream = GetResourceStream(testFile_Headers);
+
+            // Arrage: Generate a new flat file provider and then connect it to the stream of the CSV file
+            IDataProvider provider = new FlatFileProvider();
+            provider.Connect(resourceStream);
+
+            // Arrage: Provide a definition of what wants to be retrieved from the flat file
+            DataItemDefinition definition = TestDefinition(testFile_Headers);
+
+            // Act
+            DataTable data = provider.ExecuteReader(definition, ""); // Get the data
+            DataRow dataRow = (data.Rows.Count >= 2) ? data.Rows[2] : null; // Get row 3 to check the data against later
+
+            // Assert
+            Assert.True(data.Rows.Count == 5); // Should be 5 rows (6 - The Header)
+            Assert.True(data.Columns.Contains("Description Header")); // Should be a column that was found even though it had no quotes
+            Assert.True(dataRow != null && (String)dataRow["Description Header"] == "Description 3"); // The third row should have some data for the unquoted header
         }
 
         /// <summary>
         /// Generate a test definition that is common between the different test files
         /// </summary>
         /// <returns>The common data definition</returns>
-        private DataItemDefinition TestDefinition(Boolean hasHeaderRecord)
+        private DataItemDefinition TestDefinition(String testDefinition)
         {
             // Arrage: Provide a definition of what wants to be retrieved from the flat file
             DataItemDefinition definition = new DataItemDefinition();
 
-            definition.Properties.Add(new DataItemProperty() { Name = "Title", DataType = typeof(String), OridinalPosition = (hasHeaderRecord ? -1 : 0) });
-            definition.Properties.Add(new DataItemProperty() { Name = "CreatedDate", DataType = typeof(DateTime), OridinalPosition = (hasHeaderRecord ? -1 : 1) });
-            definition.Properties.Add(new DataItemProperty() { Name = "Size", DataType = typeof(Double), OridinalPosition = (hasHeaderRecord ? -1 : 2) });
-            definition.Properties.Add(new DataItemProperty() { Name = "Description", DataType = typeof(String), OridinalPosition = (hasHeaderRecord ? -1 : 3) });
-            definition.Properties.Add(new DataItemProperty() { Name = "Active", DataType = typeof(Boolean), OridinalPosition = (hasHeaderRecord ? -1 : 4) });
-            definition.HasHeaderRecord = hasHeaderRecord; // No Header Record For This Test
+            switch (testDefinition)
+            {
+                case testFile_DataTypes:
 
+                    // Definition for different data types and the data defined by ordinal position
+                    definition.Properties.Add(new DataItemProperty() { Name = "Title", DataType = typeof(String), OridinalPosition = 0 });
+                    definition.Properties.Add(new DataItemProperty() { Name = "CreatedDate", DataType = typeof(DateTime), OridinalPosition = 1 });
+                    definition.Properties.Add(new DataItemProperty() { Name = "Size", DataType = typeof(Double), OridinalPosition = 2 });
+                    definition.Properties.Add(new DataItemProperty() { Name = "Description", DataType = typeof(String), OridinalPosition = 3 });
+                    definition.Properties.Add(new DataItemProperty() { Name = "Active", DataType = typeof(Boolean), OridinalPosition = 4 });
+                    definition.HasHeaderRecord = false;
+
+                    break;
+
+                case testFile_Headers:
+
+                    // Definition for getting the data by the name of the header
+                    definition.Properties.Add(new DataItemProperty() { Name = "Title", DataType = typeof(String), OridinalPosition = -1 });
+                    definition.Properties.Add(new DataItemProperty() { Name = "Description Header", DataType = typeof(String), OridinalPosition = -1 });
+                    definition.Properties.Add(new DataItemProperty() { Name = "Value", DataType = typeof(String), OridinalPosition = -1 });
+                    definition.HasHeaderRecord = true;
+
+                    break;
+            }
+            
             // Return the definition
             return definition;
         }
