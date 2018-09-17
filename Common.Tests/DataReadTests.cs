ff3b2760d82462160588cd541e827d7d6a0050f3
TNDStudios <info@thenakeddeveloper.com>
2018-09-18 00:58:40 +0100

    Reading tests for boolean and date time properties for the helper classes

--- Common.Tests/DataReadTests.cs
+++ Common.Tests/DataReadTests.cs
@@ -1,5 +1,8 @@
 using System;
 using Xunit;
+using TNDStudios.DataPortals.Data;
+using TNDStudios.DataPortals.Helpers;
+using System.Collections.Generic;
 
 namespace TNDStudios.DataPortals.Tests.Common
 {
@@ -12,7 +15,26 @@ namespace TNDStudios.DataPortals.Tests.Common
         [Fact]
         public void DateRead()
         {
+            // Arrange
+            DataItemProperty property = new DataItemProperty()
+            {
+                DataType = typeof(DateTime),
+                Name = "DateTimeTest",
+                OridinalPosition = -1,
+                Pattern = "dd MMM yyyy"
+            };
 
+            DataItemDefinition definition = new DataItemDefinition() { };
+
+            // Act
+            Object rawValue = DataFormatHelper.ReadData("23 Oct 1984", property, definition);
+            DateTime value = (rawValue != DBNull.Value) ? (DateTime)rawValue : DateTime.MinValue;
+
+            // Assert
+            Assert.True(value != DateTime.MinValue &&
+                        value.Day == 23 &&
+                        value.Month == 10 &&
+                        value.Year == 1984);
         }
 
         /// <summary>
@@ -22,7 +44,11 @@ namespace TNDStudios.DataPortals.Tests.Common
         [Fact]
         public void StringRead()
         {
+            // Arrange
 
+            // Act
+
+            // Assert
         }
 
         /// <summary>
@@ -32,7 +58,22 @@ namespace TNDStudios.DataPortals.Tests.Common
         [Fact]
         public void BooleanRead()
         {
+            // Arrange
+            DataItemProperty property = new DataItemProperty()
+            {
+                DataType = typeof(Boolean),
+                Name = "BooleanTest",
+                OridinalPosition = -1
+            };
 
+            DataItemDefinition definition = new DataItemDefinition() { };
+
+            // Act
+            Object rawValue = DataFormatHelper.ReadData("true", property, definition);
+            Boolean value = (rawValue != DBNull.Value) ? (Boolean)rawValue : false;
+
+            // Assert
+            Assert.True(value);
         }
     }
 }
