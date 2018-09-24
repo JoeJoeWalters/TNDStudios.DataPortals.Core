ac3325706c42d1046052a73e65f0cec41d261a54
TNDStudios <info@thenakeddeveloper.com>
2018-09-25 00:16:38 +0100

    Adding in a few placeholders to start the work binding the api controllers to the Vue.js VM

---
+++ UI/Controllers/Api/TestApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Controllers.Api
+{
+    public class TestData
+    {
+        public String value { get; set; }
+    }
+
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/Api/TestData")]
+        public TestData TestData()
+        {
+            return new TestData() { value = "Test Data" };
+        }
+    }
+}
