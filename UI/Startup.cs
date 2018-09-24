549dce5243d01517f9770f157498de151c4faa83
TNDStudios <info@thenakeddeveloper.com>
2018-09-24 01:51:07 +0100

    Starting to implement Vue as the MVVM framework.

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -31,8 +31,8 @@ namespace UI
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
