dc2116029a11722978a0b043934d4838f70840d1
TNDStudios <info@thenakeddeveloper.com>
2018-09-28 14:42:24 +0100

    Get File Content from incoming IFormFile Stream

--- UI/Controllers/Api/DataItemController.cs
+++ UI/Controllers/Api/DataItemController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using TNDStudios.DataPortals.Data;
 using TNDStudios.DataPortals.UI.Controllers.RequestResponse;
 using TNDStudios.DataPortals.UI.Models.Api;
@@ -13,12 +15,18 @@ namespace TNDStudios.DataPortals.UI.Controllers.Api
     {
         [HttpPost]
         [Route("/api/data/analyse/file")]
-        public ApiResponse<DataItemModel> AnalyseFile()
+        public ApiResponse<DataItemModel> AnalyseFile([FromForm] IFormFile Upload)
         {
             // Create the response object
             DataItemModel result = new DataItemModel() { };
 
+            // Get a stream of data out of the uploaded file
+            using (MemoryStream stream = new MemoryStream())
+            {
+                Upload.CopyTo(stream);
+                Byte[] fileContent = stream.ToArray();
 
+            }
 
             // Respond with the analysed data
             return new ApiResponse<DataItemModel>() { Data = result, Success = true };
