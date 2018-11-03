cad3527bc829a6b9ab99e5eb1b5abf0ce606f215
TNDStudios <info@thenakeddeveloper.com>
2018-11-03 23:45:42 +0000

    Implementing the data definition saving / editing based on the methodology that the connection editor implemented.

--- UI/wwwroot/js/models/datadefinitions.js
+++ UI/wwwroot/js/models/datadefinitions.js
@@ -2,26 +2,81 @@
 tndStudios.models = tndStudios.models || {};
 tndStudios.models.dataDefinitions =
     {
+        // Edit Page Model
+        page: function () {
+
+            // The properties of the object
+            this.dataDefinitions = []; // The list of data definitions for this package
+            this.searchCriteria = ""; // The filter for the data definitions list
+
+            this.editor = new tndStudios.models.dataDefinitions.dataItemDefinition(null); // The editor object
+            this.editItem = null; // Reference to the item that is being edited for saving changes back to it
+            this.providerTypes = []; // The list of available provider types
+        },
+
         // Data Item Definition Model
-        dataItemDefinition: function () {
+        dataItemDefinition: function (data) {
+            
+            // General Properties
+            this.id = null;
+            this.name = '';
+            this.description = '';
 
-            // System items
-            this.dirty = false;
-
-            // Model Properties
+            // Item properties
             this.itemProperties = [];
             this.propertyBag = [];
             this.culture = '';
             this.encodingFormat = '';
             this.connections = [];
+
+            // Copy the content of this data item definition from another data item definition
+            // e.g. when editing in a secondary editor object
+            this.fromObject = function (fromObject) {
+
+                // Clear the object first (just in case)
+                this.clear();
+
+                // Start copying the data from the other object
+                this.id = fromObject.id;
+                this.name = fromObject.name;
+                this.description = fromObject.description;
+            }
+
+            // Create a formatted object that can be passed to the server
+            this.toObject = function () {
+
+                var result =
+                {
+                    Id: this.id,
+                    Name: this.name,
+                    Description: this.description
+                };
+
+                return result;
+            }
+
+            // Clear this connection object (i.e. make it ready for editing)
+            this.clear = function () {
+
+                // Clear the properties
+                this.id = null;
+                this.name = '';
+                this.description = '';
+                this.itemProperties = [];
+                this.propertyBag = [];
+                this.culture = '';
+                this.encodingFormat = '';
+                this.connections = [];
+            }
+
+            // Any data passed in?
+            if (data) {
+                this.fromObject(data); // Assign the data to this object
+            }
         },
 
         // Data Item Property Model
         dataItemProperty: function () {
-
-            // System Items
-            this.removed = false;
-            this.dirty = false;
 
             // Model Properties
             this.dataType = 'System.String';
--- UI/wwwroot/js/pages/datadefinitions/index.js
+++ UI/wwwroot/js/pages/datadefinitions/index.js
@@ -1,41 +1,199 @@
 ﻿var app = new Vue({
     el: '#contentcontainer',
     data: {
-        dataDefinitions: [],
-        dataDefinition: new tndStudios.models.dataDefinitions.dataItemDefinition()
+        page: new tndStudios.models.dataDefinitions.page()
     },
     computed: {
+
+        // Searchable list of the data definitions
+        filteredDataDefinitions() {
+            return this.page.dataDefinitions.filter(function (item) {
+                return item.name.toLowerCase().indexOf(app.page.searchCriteria.toLowerCase()) > -1
+            });
+        },
+
+        // Is the editor item saved?
+        isSavedVisible() {
+
+            if (this.page.editor.id != null &&
+                this.page.editor.id != "")
+                return "visible";
+            else
+                return "hidden";
+        }
     },
     methods: {
 
         // Add a new item to the definition list
-        add: function () {
+        newDataDefinition: function () {
 
-            // Push the new item
-            app.dataDefinitions.push(new tndStudios.models.dataDefinitions.dataItemDefinition());
+            // Clear the editor
+            app.page.editor.clear();
+            app.page.editItem = null; // No longer attached to an editing object
         },
 
+        // Edit an existing item by assigning it to the editor object
+        edit: function (editItem) {
+
+            // Copy the data to the data definition editor from the selected object
+            app.page.editor.fromObject(editItem);
+            app.page.editItem = editItem; // Reference to the origional item being edited
+        },
+
+        // Delete the current data definition
+        deleteDataDefinition: function () {
+
+            // Get the editor id field
+            var idString = "";
+            if (app.page.editor && app.page.editor.id) {
+                idString = app.page.editor.id;
+            }
+
+            // Make sure this is a real data definition just in case
+            // someone clicked the delete before it was saved
+            if (idString != "") {
+
+                tndStudios.utils.api.call(
+                    '/api/data/definition/' + idString,
+                    'DELETE',
+                    null,
+                    app.deleteSuccess,
+                    app.deleteFailure
+                );
+            }
+            else
+                tndStudios.utils.ui.notify(0, 'Cannot Delete An Item That Is Not Saved Yet');
+
+        },
+
+        // Test was successful
+        deleteSuccess: function (data) {
+
+            // Clear the editor (as it was showing the data definition when it was deleted)
+            app.page.editor.clear();
+
+            // Remove the item from the data definitions list
+            app.page.dataDefinitions = app.page.dataDefinitions.filter(
+                function (dataDefinition) {
+                    return dataDefinition.id !== app.page.editItem.id;
+                });
+
+            app.page.editItem = null; // No longer attached to an editing object
+
+            // Notify the user
+            tndStudios.utils.ui.notify(1, "Data Definition Deleted Successfully");
+        },
+
+        // Test failed
+        deleteFailure: function () {
+
+            // Notify the user
+            tndStudios.utils.ui.notify(0, "Data Definition Deletion Failed");
+        },
+
+
+        // Save the contents of the editor object 
+        saveDataDefinition: function () {
+
+            // Is the form valid?
+            if ($("#editorForm").valid()) {
+
+                // The the api call to save the data definition
+                tndStudios.utils.api.call(
+                    '/api/data/definition',
+                    'POST',
+                    app.page.editor.toObject(),
+                    app.saveSuccess,
+                    app.saveFailure
+                );
+            }
+
+        },
+
+        // Save was successful, assign the appropriate items
+        saveSuccess: function (data) {
+
+            // Some data came back?
+            if (data.data) {
+
+                // Update the editor itself (possibily with new links or id if a new item)
+                app.page.editor.fromObject(data.data);
+
+                // Were we editing an existing item?
+                if (app.page.editItem != null) {
+
+                    // Update the existing item
+                    app.page.editItem.fromObject(data.data);
+                }
+                else {
+
+                    // Add the new item to the list
+                    app.page.editItem = new tndStudios.models.dataDefinitions.dataItemDefinition(data.data);
+                    app.page.dataDefinitions.push(app.page.editItem);
+                }
+
+                // Notify the user
+                tndStudios.utils.ui.notify(1, "Data Definition Saved ('" + data.data.name + "')");
+            };
+        },
+
+        // Save was unsuccessful, inform the user
+        saveFailure: function () {
+
+            // Notify the user
+            tndStudios.utils.ui.notify(0, "Data Definition Could Not Be Saved");
+        },
+        
+        // Start the load process
         load: function () {
+            
+            // Start loading the datae definitions list
+            app.loadDataDefinitions();
+        },
+
+        // load data definitions from the server
+        loadDataDefinitions: function () {
+
+            // Start the api call to load the data definitions
             tndStudios.utils.api.call(
                 '/api/data/definition',
                 'GET',
                 null,
-                app.loadSuccess,
-                app.loadFailure);
+                app.loadDataDefinitionsSuccess,
+                app.loadDataDefinitionsFailure);
         },
 
         // Load was successful, assign the data
-        loadSuccess: function (data) {
+        loadDataDefinitionsSuccess: function (data) {
             if (data.data) {
-                app.dataDefinitions = data.data; // Assign the Json package to the data definition
+
+                app.page.dataDefinitions = []; // clear the data definitions array
+
+                // Add the data definition objects back in with wrapper for additional functions
+                data.data.forEach(function (dataDefinition) {
+                    app.page.dataDefinitions.push(new tndStudios.models.dataDefinitions.dataItemDefinition(dataDefinition)); // Assign the Json package to the data definition
+                });
             };
         },
 
         // Load was unsuccessful, inform the user
-        loadFailure: function () {
-            alert('Failed to retrieve existing data definitions list')
+        loadDataDefinitionsFailure: function () {
         },
     }
 });
 
+// Create the validation rules for the main editor form
+$("#editorForm").validate(
+    {
+        rules:
+        {
+            name: { required: true }
+        },
+        messages:
+        {
+            name: "Name Of Data Definition Required"
+        }
+    });
+
+// Start the load process to initialise the form
 app.load();
