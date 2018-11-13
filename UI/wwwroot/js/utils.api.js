00596f77f2fbdd9998578eca231574cc211e5da3
TNDStudios <info@thenakeddeveloper.com>
2018-11-13 00:12:05 +0000

    Added very rough progress indicator for long running actions

--- UI/wwwroot/js/utils.api.js
+++ UI/wwwroot/js/utils.api.js
@@ -4,6 +4,10 @@ tndStudios.utils.api =
     {
         call: function (url, method, request, successCallBack, failureCallBack) {
 
+            // Start the progress spinner
+            tndStudios.utils.ui.progress(true);
+
+            // Make the ajax call
             $.ajax({
                 type: method,
                 url: url,
@@ -12,12 +16,22 @@ tndStudios.utils.api =
                 dataType: 'json',
                 contentType: 'application/json; charset=utf-8',
                 success: function (data, status, jqXHR) {
+
+                    // Stop the progress spinner
+                    tndStudios.utils.ui.progress(false);
+
+                    // Success?
                     if (data.success)
                         successCallBack(data);
                     else
                         failureCallBack();
                 },
                 error: function (jgXHR, status) {
+
+                    // Stop the progress spinner
+                    tndStudios.utils.ui.progress(false);
+
+                    // Do the failure call
                     failureCallBack();
                 }
             });
--- UI/wwwroot/js/utils.ui.js
+++ UI/wwwroot/js/utils.ui.js
@@ -5,6 +5,7 @@ var tndStudios = tndStudios || {};
 tndStudios.utils = tndStudios.utils || {};
 tndStudios.utils.ui =
     {
+        // Notify the user of alerts / messages
         notify: function (alertType, value) {
 
             var alertTranslated = 'success'; // Default alert type
@@ -21,5 +22,27 @@ tndStudios.utils.ui =
 
             // Do the notify
             $.notify({ message: value }, { type: alertTranslated });
+        },
+
+        // Inform the user that there is some progress going on
+        progress: function (on) {
+            
+            var progressSection = $("#progressBar");
+
+            // Switching on or off?
+            if (on) {
+                progressSection.removeClass("invisible");
+                progressSection.removeClass("d-none");
+                progressSection.addClass("visible");
+                progressSection.addClass("d-inline");
+            }
+            else {
+                progressSection.addClass("invisible");
+                progressSection.addClass("d-none");
+                progressSection.removeClass("visible");
+                progressSection.removeClass("d-inline");
+            }
+
         }
+
     };
