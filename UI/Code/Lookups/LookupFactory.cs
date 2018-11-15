8a5f1a70b6eb1c5f7db9c8f9a698ad69b2c08647
TNDStudios <info@thenakeddeveloper.com>
2018-11-15 01:59:23 +0000

    Lookup Endpoint & Core System Api Setup

---
+++ UI/Code/Lookups/LookupFactory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNDStudios.DataPortals.UI
+{
+    /// <summary>
+    /// Enumerations for lookups
+    /// </summary>
+    public enum LookupFactoryType : Int32
+    {
+        Unknown = 0,
+        Encoding = 1,
+        Culture = 2
+    }
+
+    /// <summary>
+    /// Factory class to handle getting a list of certain lookups
+    /// </summary>
+    public class LookupFactory
+    {
+        /// <summary>
+        /// Get a lookup of a given type
+        /// </summary>
+        /// <param name="type">The lookup type</param>
+        /// <returns>The lookup dictionary</returns>
+        public List<KeyValuePair<String, String>> Get(LookupFactoryType type)
+        {
+            // Result
+            List<KeyValuePair<String, String>> result = new List<KeyValuePair<String, String>>();
+
+            // Check the type to get the right lookup type
+            switch (type)
+            {
+                case LookupFactoryType.Culture:
+
+                    // Get the list of cultures available
+                    result = CultureInfo.GetCultures(CultureTypes.AllCultures)
+                        .Select(culture =>
+                            new KeyValuePair<String, String>(culture.Name, culture.DisplayName)
+                            ).ToList();
+
+                    // Add the invariant culture
+                    result.Add(new KeyValuePair<String, String>("", "Invariant"));
+
+                    break;
+
+                case LookupFactoryType.Encoding:
+
+                    // Get the list of encodings
+                    result = Encoding.GetEncodings()
+                        .Select(encoding =>
+                            new KeyValuePair<String, String>(encoding.Name, encoding.DisplayName)
+                        ).ToList();
+
+                    break;
+            }
+
+            // Return the result
+            return result;
+        }
+    }
+}
---
+++ UI/Controllers/Api/SystemApiController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace TNDStudios.DataPortals.UI.Controllers.Api
+{
+    /// <summary>
+    /// Api Controller that managed API's are written to
+    /// </summary>
+    [ApiController]
+    public class SystemApiController : ApiControllerBase
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public SystemApiController() : base()
+        {
+        }
+
+        /// <summary>
+        /// Get a list from the given lookup
+        /// </summary>
+        /// <returns>A list of a given type</returns>
+        [HttpGet]
+        [Route("/api/system/lookup/{id}")]
+        public ApiResponse<List<KeyValuePair<String, String>>> Get(LookupFactoryType id)
+            => new ApiResponse<List<KeyValuePair<String, String>>>()
+            {
+                Data = (new LookupFactory()).Get(id),
+                Success = true
+            };
+    }
+}
