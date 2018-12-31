c16a9b330a410b69d44f9dd22d5dc18dc5770c5b
TNDStudios <info@thenakeddeveloper.com>
2018-12-31 22:31:09 +0000

    Populate Connection Model Test

--- Tests/UI.Tests/ConnectionApiTests.cs
+++ Tests/UI.Tests/ConnectionApiTests.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TNDStudios.DataPortals.Data;
+using TNDStudios.DataPortals.Repositories;
+using TNDStudios.DataPortals.Security;
+using TNDStudios.DataPortals.UI.Controllers.Api.Helpers;
+using TNDStudios.DataPortals.UI.Models.Api;
 using Xunit;
 
 namespace TNDStudios.DataPortals.Tests.UI
@@ -14,7 +20,42 @@ namespace TNDStudios.DataPortals.Tests.UI
         [Fact]
         public void Populate_ConnectionModel()
         {
-            Assert.True(false);
+            // Arrange
+            Guid credentialsId = Guid.NewGuid(); // Set the fixed id of the credentials
+
+            // Create a new package to pick the credentials up from in the populate routine
+            Package package = new Package()
+            {
+                CredentialsStore = new List<Credentials>()
+                {
+                    new Credentials(){ Id = credentialsId, Name = "Dummy Credentials" }
+                }
+            };
+
+            // Create a new connection model without the populated fields
+            DataConnectionModel model = new DataConnectionModel()
+            {
+                Credentials = 
+                    new KeyValuePair<Guid, string>
+                        (credentialsId, String.Empty),
+                ProviderType = (Int32)DataProviderType.SQLProvider
+            };
+
+            // Set up the mapper to map any needed objects
+            Mapper.Reset(); // Only used for tests, clear other mappings
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfiles("TNDStudios.DataPortals.UI");
+            });
+            IMapper mapper = new Mapper(Mapper.Configuration);
+
+            // Act
+            model = (new ConnectionApiHelpers())
+                .PopulateModel(mapper, package, model);
+
+            // Assert
+            Assert.True(model.Credentials.Value != String.Empty);
+            Assert.True(model.ProviderData != null);
         }
     }
 }
