0217fe33220042cf00d48372423d92252499137f
TNDStudios <info@thenakeddeveloper.com>
2019-02-04 00:55:18 +0000

    Tidy Up

--- UI/Controllers/Api/PackageApiController.cs
+++ UI/Controllers/Api/PackageApiController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TNDStudios.DataPortals.Repositories;
 using TNDStudios.DataPortals.UI;
